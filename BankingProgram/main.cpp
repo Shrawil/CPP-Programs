03ac72eb20c8d458c8f6aaf0a77ee580084c37b9
Shrawil Srivastava <shrawilsrivastavaa@gmail.com>
2026-05-08 14:07:25 +0530

    Implement main function for banking program

--- BankingProgram/main.cpp
+++ BankingProgram/main.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include"handleInput.h"
 
+int main() {
+	using std::cout;
+
+    bool logged = false;
+    bool noAcc = true;
+    bool running = true;
+
+    acc user;
+
+	while(running) {
+		user = handle(logged, noAcc, user);
+	}
+	
+    return 0;
+}
